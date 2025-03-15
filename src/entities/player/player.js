64d0ae745933686295bf509cd17a494fda359db9
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-03-15 15:32:59 -0300

    ref: add motion methods

--- src/entities/player/player.js
+++ src/entities/player/player.js
@@ -11,34 +11,15 @@ export class Player {
 			x: 0,
 			y: 0,
 		}
+
+		this.interactionRequired =
+			!!this.game.sceneManager?.activeScene?.dialogManager.currentOptions
 	}
 
-	update(keys) {
-		const interactingWithNPC =
-			!!this.game.sceneManager.activeScene.dialogManager.currentOptions
-
-		const moveUp = (keys?.['ArrowUp'] || keys?.['w']) && !interactingWithNPC
-		const moveBot = (keys?.['ArrowDown'] || keys?.['s']) && !interactingWithNPC
-
-		const moveLeft = (keys?.['ArrowLeft'] || keys?.['a']) && !interactingWithNPC
-		const moveRight =
-			(keys?.['ArrowRight'] || keys?.['d']) && !interactingWithNPC
-
-		if (moveUp) {
-			this.velocity.y = -this.speed
-		} else if (moveBot) {
-			this.velocity.y = this.speed
-		} else {
-			this.velocity.y = 0
-		}
-
-		if (moveLeft) {
-			this.velocity.x = -this.speed
-		} else if (moveRight) {
-			this.velocity.x = this.speed
-		} else {
-			this.velocity.x = 0
-		}
+	update() {
+		this.updateInputs()
+		this.horizontalMovement()
+		this.verticalMovemet()
 
 		this.y += this.velocity.y
 		this.x += this.velocity.x
@@ -47,5 +28,40 @@ export class Player {
 	draw(ctx, camera) {
 		ctx.fillStyle = this.color
 		ctx.fillRect(this.x - camera.x, this.y - camera.y, this.width, this.height)
+	}
+
+	horizontalMovement() {
+		const moveLeft = this.direction.left && !this.interactionRequired
+		const moveRight = this.direction.right && !this.interactionRequired
+
+		if (moveLeft) {
+			this.velocity.x = -this.speed
+		} else if (moveRight) {
+			this.velocity.x = this.speed
+		} else {
+			this.velocity.x = 0
+		}
+	}
+
+	verticalMovemet() {
+		const moveUp = this.direction.up && !this.interactionRequired
+		const moveBot = this.direction.bottom && !this.interactionRequired
+
+		if (moveUp) {
+			this.velocity.y = -this.speed
+		} else if (moveBot) {
+			this.velocity.y = this.speed
+		} else {
+			this.velocity.y = 0
+		}
+	}
+
+	updateInputs() {
+		this.direction = {
+			left: this.game.keys?.['ArrowLeft'] || this.game.keys?.['a'],
+			right: this.game.keys?.['ArrowRight'] || this.game.keys?.['d'],
+			up: this.game.keys?.['ArrowUp'] || this.game.keys?.['w'],
+			bottom: this.game.keys?.['ArrowDown'] || this.game.keys?.['s'],
+		}
 	}
 }

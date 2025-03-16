d42da7185ccdc0c22dffda8fa1acab9bfec2049b
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-03-16 17:17:05 -0300

    ref: menu and creation scenes now uses MenuButton component

--- src/scenes/creation/NewGameScene.js
+++ src/scenes/creation/NewGameScene.js
@@ -1,86 +1,61 @@
 import { UIManager } from '../../core/managers/UIManager.js'
+import { MenuButton } from '../../components/MenuButton.js'
 
-import { Button } from '../../components/Button.js'
-import {
-	btn1,
-	btn2,
-	btnFz,
-	btnHeight,
-	btnPlr,
-	btnPtb,
-	btnWidth,
-	btnX,
-} from '../constants.js'
 import { SCENES } from '../../core/constants.js'
+import { loadImage } from '../../core/utils.js'
+
+const BG_IMG = '/src/scenes/creation/assets/background.jpg'
 
 export class NewGameScene {
 	constructor(game) {
 		this.game = game
 		this.uiManager = new UIManager(this.game)
 
-		// this.hoverInitButton = false
-		// this.hoverBackButton = false
-
-		/* Screen */
-		this.backgroundImg = new Image()
-		this.backgroundImg.src = '/src/scenes/creation/assets/background.jpg'
-		this.initGame_button = new Button(
-			btnX,
-			btn1.y,
-			btnWidth,
-			btnHeight,
-			'Comenzar',
-			btnFz,
-			btnPlr,
-			btnPtb
-		)
-		this.back_button = new Button(
-			btnX,
-			btn2.y,
-			btnWidth,
-			btnHeight,
-			'Atras',
-			btnFz,
-			btnPlr,
-			btnPtb
-		)
-
-		this.initGame_button.onClick = () => {
-			this.game.sceneManager.changeScene(SCENES.city.valeris)
-		}
-		this.back_button.onClick = () => {
-			this.game.sceneManager.changeScene(SCENES.menu)
-		}
-		this.uiManager.addComponent(this.initGame_button)
-		this.uiManager.addComponent(this.back_button)
+		this.loaded = false
+		loadImage(BG_IMG).then((res) => {
+			this.bg = res
+			this.loaded = true
+		})
 	}
 
-	onEnter() {
-		console.log('Entrando en la escena: Creacion de Partida')
-	}
-	onExit() {
-		console.log('Saliendo de la escena: Creacion de Partida')
-	}
-	handleClick(mouseX, mouseY, e) {
-		this.uiManager.handleClick(mouseX, mouseY, e)
-	}
-
-	update(deltaTime) {
-		// Lógica de actualización (si aplica)
-	}
+	update() {}
 
 	render() {
 		const { ctx } = this.game
 
-		ctx.fillStyle = 'black'
-		ctx.fillRect(0, 0, this.game.canvas.width, this.game.canvas.height)
-
-		this.game.sceneManager.setBackground(this.backgroundImg)
-		this.uiManager.renderComponents()
+		if (this.loaded) {
+			this.game.sceneManager.setBackground(this.bg)
+			this.uiManager.renderComponents()
+		} else {
+			// black screen
+			ctx.fillStyle = 'black'
+			ctx.fillRect(0, 0, this.game.canvas.width, this.game.canvas.height)
+		}
 	}
 
+	/* Load/Unload */
+	onEnter() {
+		const goValeris = () =>
+			this.game.sceneManager.changeScene(SCENES.city.valeris)
+		const goMenu = () => this.game.sceneManager.changeScene(SCENES.menu)
+
+		this.initGame_button = new MenuButton(1, 'Comenzar')
+		this.initGame_button.onEvent(goValeris)
+		this.uiManager.addComponent(this.initGame_button)
+
+		this.back_button = new MenuButton(2, 'Menu')
+		this.back_button.onEvent(goMenu)
+		this.uiManager.addComponent(this.back_button)
+	}
+	onExit() {
+		this.uiManager.components.clear()
+	}
+
+	/* Events */
+	handleClick(mouseX, mouseY, e) {
+		this.uiManager.handleClick(mouseX, mouseY, e)
+	}
 	mouseMove(mouseX, mouseY, e) {
-		this.initGame_button.handleHover(mouseX, mouseY)
-		this.back_button.handleHover(mouseX, mouseY)
+		this.uiManager.mouseMove(mouseX, mouseY, e)
 	}
 }
--- src/scenes/menu/MenuScene.js
+++ src/scenes/menu/MenuScene.js
@@ -1,81 +1,56 @@
-import { Button } from '../../components/Button.js'
-import { SCENES } from '../../core/constants.js'
 import { UIManager } from '../../core/managers/UIManager.js'
+import { MenuButton } from '../../components/MenuButton.js'
 
-import {
-	btn1,
-	btn2,
-	btnFz,
-	btnHeight,
-	btnPlr,
-	btnPtb,
-	btnWidth,
-	btnX,
-} from '../constants.js'
+import { SCENES } from '../../core/constants.js'
+import { loadImage } from '../../core/utils.js'
+
+const BG_IMG = '/src/scenes/menu/assets/background.jpg'
 
 export class MenuScene {
 	constructor(game) {
 		this.game = game
 		this.uiManager = new UIManager(this.game)
 
-		this.backgroundImg = new Image()
-		this.backgroundImg.src = '/src/scenes/menu/assets/background.jpg'
+		this.loaded = false
+		loadImage(BG_IMG).then((res) => {
+			this.bg = res
+			this.loaded = true
+		})
 	}
 
-	onEnter() {
-		console.log('Entrando en la escena: Menú Principal')
-		// Crear botones
-		this.newGameButton = new Button(
-			btnX,
-			btn1.y,
-			btnWidth,
-			btnHeight,
-			'Crear Partida',
-			btnFz,
-			btnPlr,
-			btnPtb
-		)
-		this.exitButton = new Button(
-			btnX,
-			btn2.y,
-			btnWidth,
-			btnHeight,
-			'Salir',
-			btnFz,
-			btnPlr,
-			btnPtb
-		)
+	update() {}
 
-		this.newGameButton.onClick = () => {
-			this.game.sceneManager.changeScene(SCENES.creation)
+	render() {
+		if (this.loaded) {
+			this.game.sceneManager.setBackground(this.bg)
+			this.uiManager.renderComponents()
+		} else {
+			// renderizar pantalla de carga
 		}
-		this.exitButton.onClick = () => {
-			console.log('exit game')
-		}
-		// Añadir botones al UIManager
+	}
+
+	/* Load/Unload */
+	onEnter() {
+		const goCreate = () => this.game.sceneManager.changeScene(SCENES.creation)
+
+		/* BOTONES DE INTERFAZ */
+		this.newGameButton = new MenuButton(1, 'Crear Partida')
+		this.newGameButton.onEvent(goCreate)
 		this.uiManager.addComponent(this.newGameButton)
+
+		this.exitButton = new MenuButton(2, 'Salir')
+		this.exitButton.onEvent(() => console.log('exit game'))
 		this.uiManager.addComponent(this.exitButton)
 	}
 	onExit() {
-		console.log('Saliendo de la escena: Menú Principal')
-		// Limpiar el UIManager de esta escena
 		this.uiManager.components.clear()
 	}
+
+	/* Events */
 	handleClick(mouseX, mouseY, e) {
 		this.uiManager.handleClick(mouseX, mouseY, e)
 	}
 	mouseMove(mouseX, mouseY, e) {
-		this.newGameButton.handleHover(mouseX, mouseY)
-		this.exitButton.handleHover(mouseX, mouseY)
-	}
-
-	update(deltaTime) {
-		// Lógica de actualización (si aplica)
-	}
-
-	render() {
-		this.game.sceneManager.setBackground(this.backgroundImg)
-		// Renderizar componentes del UIManager
-		this.uiManager.renderComponents()
+		this.uiManager.mouseMove(mouseX, mouseY, e)
 	}
 }

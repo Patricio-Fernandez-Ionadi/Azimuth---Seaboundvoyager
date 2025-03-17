9deb4580c5a25243e1bf6622e4722ee2a602db2f
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-03-17 00:49:32 -0300

    feat: implement NPC trading functionality and enhance interaction logic

--- src/scenes/cities/PuertoValeris.js
+++ src/scenes/cities/PuertoValeris.js
@@ -1,102 +1,196 @@
-// src/scenes/ValerisScene.js
-import { Camera } from '../../core/Camera.js'
-import { UIManager } from '../../core/managers/UIManager.js'
-import { DialogManager } from './DialogManager.js'
+import { CameraManager } from '../../core/managers/CameraManager.js'
+import { DialogManager } from '../../core/managers/DialogManager.js'
 
 import { NPC } from '../../entities/npcs/NPC.js'
 import { Button } from '../../components/Button.js'
+import { TradeWindow } from '../../components/TradeWindow.js'
 
 import { SCENES } from '../../core/constants.js'
+import { checkCollisions } from '../../core/utils.js'
+import { Item } from '../../components/Item.js'
+
+const MOCK_WIDTH = 1984 /* 124 * 16 */
+const MOCK_HEIGHT = 1088 /* 68 * 16 */
 
 const NPCS = [
-	new NPC(200, 200, 'blue', [
-		'¡Bienvenido a Puerto Valeris!',
-		'¿Necesitas algo, marinero?',
-		'Puedo ofrecerte algunos suministros.',
-		'¡Buena suerte en tu viaje!',
-	]),
-	new NPC(300, 300, 'green', [
-		'Hola, forastero.',
-		'Este lugar es peligroso.',
-		'Ten cuidado con los piratas.',
-	]),
-	new NPC(100, 200, 'yellow', [
-		'¡Bienvenido a mi tienda!',
-		{
-			message: 'Puedo ofrecerte algunos suministros.',
-			options: [
-				{
-					text: 'Comerciar',
-					callback: () => console.log('Abrir ventana de comercio'),
-				},
-				{ text: 'Salir', callback: () => console.log('Salir del diálogo') },
-			],
-		},
-	]),
+	{
+		x: 340,
+		y: 320,
+		color: 'blue',
+		speech: [
+			'¡Bienvenido a Puerto Valeris!',
+			'¿Necesitas algo, marinero?',
+			{
+				message: 'Puedo ofrecerte algunos suministros.',
+				options: [
+					{
+						text: 'Comerciar',
+						callback: () => console.log('Abrir ventana de comercio'),
+					},
+					{ text: 'Salir', callback: () => console.log('Salir del diálogo') },
+				],
+			},
+			'¡Buena suerte en tu viaje!',
+		],
+	},
+	{
+		x: 300,
+		y: 260,
+		color: 'green',
+		speech: [
+			'Hola, forastero.',
+			'Este lugar es peligroso.',
+			'Ten cuidado con los piratas.',
+		],
+	},
+	{
+		x: 380,
+		y: 50,
+		color: 'yellow',
+		speech: [
+			'¡Bienvenido a mi tienda!',
+			{
+				message: 'Puedo ofrecerte algunos suministros.',
+				options: [
+					{
+						text: 'Comerciar',
+						callback: () => console.log('Abrir ventana de comercio'),
+					},
+					{ text: 'Salir', callback: () => console.log('Salir del diálogo') },
+				],
+			},
+		],
+		inventory: [
+			new Item(
+				1,
+				'Gold Key',
+				'Una llave unica, debe ser para algo valioso.',
+				'/src/components/assets/items/key_gold_item.png',
+				16
+			),
+			new Item(
+				4,
+				'Mapa de la region',
+				'Contiene informacion valiosa de la zona.',
+				'/src/components/assets/items/map_item.png',
+				1
+			),
+			new Item(
+				5,
+				'Macoña',
+				'Para relajarse.',
+				'/src/components/assets/items/weed_item.png',
+				16
+			),
+		],
+	},
 ]
+
+const building = {
+	x: 350,
+	y: 100,
+	width: 16 * 32,
+	height: 200,
+	color: 'white',
+	id: 1,
+}
+const door = {
+	x: 350 + 16 * 14,
+	y: 210,
+	width: 64,
+	height: 90,
+	color: 'red',
+	id: 2,
+}
+const mockStructure = (ctx, camera, config) => {
+	const { x, y, width, height, color } = config
+
+	let newX = x - camera.x
+	let newY = y - camera.y
+
+	ctx.save()
+	ctx.fillStyle = color
+	ctx.fillRect(newX, newY, width, height)
+	ctx.restore()
+}
 
 export class PuertoValerisScene {
 	constructor(game) {
 		this.game = game
-		this.keys = this.game.keys
-		this.uiManager = new UIManager(this.game)
+		this.eventSystem = this.game.eventSystem
+		this.keys = this.game.keyboard
+		this.camera = new CameraManager(this.game)
+		this.dialogManager = new DialogManager(this.game, this)
 
-		this.camera = new Camera(this.game)
-		this.camera.setMapBounds(1984 /* 124 * 16 */, 1088 /* 68 * 16 */)
+		/* DEV */
+		this.collisions = []
 
-		this.npcs = NPCS.map(
-			(npcData) =>
-				new NPC(
-					npcData.x,
-					npcData.y,
-					npcData.color,
-					npcData.dialogs,
-					this.game.eventSystem
-				)
-		)
-		// Gestor de diálogos
-		this.dialogManager = new DialogManager(this.game)
-
-		// Suscripcion a eventos
-		this.game.eventSystem.on('npcInteracted', (npc) => {
-			this.dialogManager.startDialogue(npc)
-		})
-		this.game.eventSystem.on('interactionEnded', (npc) => {
-			console.log(`Fin de interacción con NPC en (${npc.x}, ${npc.y})`)
-		})
+		this.#init()
 	}
 
 	update() {
+		const { ctx } = this.game
 		this.checkNPCInteraction()
 		this.handleNPCOptions()
 
-		this.game.player.update(this.keys)
+		/* mock level objects for collisions */
+		this.collisions.forEach((c) => {
+			mockStructure(ctx, this.camera, c)
+			const side = checkCollisions(this.game.player, c)
+			if (side) {
+				this.eventSystem.emit('stopPlayerMotion', { side, object: c })
+
+				// collisionando con la puerta
+				if (side === 'top' && c.id === 2) {
+					const pl = this.game.player.x + this.game.player.width
+					const pr = this.game.player.x
+					const cl = c.x + c.width
+					const cr = c.x
+
+					if (pl < cl && pr > cr) {
+						console.log('enter door')
+					}
+				}
+			}
+		})
+		/* mock end */
+
+		this.game.player.update()
 		this.camera.update()
 
 		if (
-			!this.keys['w'] &&
-			!this.keys['s'] &&
-			!this.keys['q'] &&
-			!this.keys['e']
+			!this.keys.onPress.w &&
+			!this.keys.onPress.s &&
+			!this.keys.onPress.q &&
+			!this.keys.onPress.e
 		) {
 			this.keyPressed = false
+		}
+
+		if (this.keys.onPress.escape && this.tradeWindow) {
+			this.tradeWindow.close()
 		}
 	}
 
 	render() {
 		const { ctx } = this.game
 
-		// Dibujar NPCs
-		this.npcs.forEach((npc) => npc.draw(ctx, this.camera))
-
 		this.game.player.draw(ctx, this.camera)
 
-		// Renderizar UI
-		this.uiManager.renderComponents(ctx)
+		// mostrar boton de mapa cuando el inventario esta cerrado
+		if (!this.game.player.inventory.isOpen) {
+			this.map_button.draw(ctx)
+		}
 
 		// Renderizar diálogo activo
 		this.dialogManager.renderDialogBox()
 		this.dialogManager.renderOptionsBox()
+
+		if (this.tradeWindow && this.game.player.inventory.tradeMode) {
+			this.tradeWindow.draw(ctx)
+		} else {
+			this.renderLevelInterface(ctx)
+		}
 	}
 
 	checkNPCInteraction() {
@@ -109,7 +203,7 @@ export class PuertoValerisScene {
 				Math.abs(playerX - npc.x) < 50 && Math.abs(playerY - npc.y) < 50 // Jugador cerca del NPC
 
 			if (canTalk) {
-				if (this.keys['e'] && !this.keyPressed) {
+				if (this.keys.onPress.e && !this.keyPressed) {
 					this.keyPressed = true
 					// si esta inactivo, interactuar
 					if (!npc.isInteracting) npc.interact()
@@ -125,13 +219,13 @@ export class PuertoValerisScene {
 	handleNPCOptions() {
 		// Manejar entrada para selección de opciones
 		if (this.dialogManager.currentNPC) {
-			if (this.keys['w'] && !this.keyPressed) {
+			if (this.keys.onPress.w && !this.keyPressed) {
 				this.keyPressed = true
 				this.dialogManager.selectedOptionIndex = Math.max(
 					0,
 					this.dialogManager.selectedOptionIndex - 1
 				)
-			} else if (this.keys['s'] && !this.keyPressed) {
+			} else if (this.keys.onPress.s && !this.keyPressed) {
 				this.keyPressed = true
 				const currentDialog =
 					this.dialogManager.currentNPC.dialogs[
@@ -143,14 +237,50 @@ export class PuertoValerisScene {
 						this.dialogManager.selectedOptionIndex + 1
 					)
 				}
-			} else if (this.keys['q'] && !this.keyPressed) {
+			} else if (this.keys.onPress.q && !this.keyPressed) {
 				this.keyPressed = true
 				this.dialogManager.selectOption(this.dialogManager.selectedOptionIndex)
 			}
 		}
 	}
 
+	renderLevelInterface(ctx) {
+		ctx.fillStyle = 'white'
+		ctx.font = '14px Arial'
+		ctx.fillText(`Oro: ${this.game.player.resources.gold}`, 10, 20)
+	}
+
 	/* Load/Unload */
+	#init() {
+		this.camera.setMapBounds(MOCK_WIDTH, MOCK_HEIGHT)
+		this.npcs = NPCS.map(
+			(data) =>
+				new NPC(
+					data.x,
+					data.y,
+					data.color,
+					data.speech,
+					data.inventory ? data.inventory : [],
+					this.game
+				)
+		)
+
+		// Agregamos objetos colisionables por el jugador
+		this.npcs.forEach((npc) => this.collisions.push(npc))
+		// this.collisions.push(building)
+		// this.collisions.push(door)
+
+		/* Suscripcion a eventos */
+		this.eventSystem.on('npcInteracted', (npc) => {
+			this.dialogManager.startDialogue(npc)
+		})
+		this.eventSystem.on('interactionEnded', (npc) => {
+			console.log(`Fin de interacción con NPC en (${npc.x}, ${npc.y})`)
+		})
+		this.eventSystem.on('playerTradeWindowOpen', ({ player, npc }) => {
+			this.tradeWindow = new TradeWindow(this.game, player, npc)
+		})
+	}
 	onEnter() {
 		// Configurar la cámara para seguir al jugador
 		if (this.game.player && this.camera.target !== this.game.player) {
@@ -170,30 +300,31 @@ export class PuertoValerisScene {
 		this.map_button.onClick = () => {
 			this.game.sceneManager.changeScene(SCENES.map)
 		}
-		this.uiManager.addComponent(this.map_button)
 	}
 	onExit() {}
 
 	/* Events */
 	handleClick(mouseX, mouseY, e) {
-		const camX = mouseX + this.camera.x
-		const camY = mouseY + this.camera.y
-
-		// Verificar si el jugador hizo clic en un NPC
-		for (const npc of this.npcs) {
-			if (
-				camX >= npc.x &&
-				camX <= npc.x + npc.width &&
-				camY >= npc.y &&
-				camY <= npc.y + npc.height
-			) {
-				npc.interact(this.player)
-				return
-			}
+		if (!this.game.player.inventory.isOpen) {
+			this.map_button.handleClick(mouseX, mouseY, e)
 		}
-		this.uiManager.handleClick(mouseX, mouseY, e)
+	}
+	mouseDown(mouseX, mouseY, e) {
+		if (this.tradeWindow) {
+			this.tradeWindow.mouseDown(mouseX, mouseY, e)
+		}
 	}
 	mouseMove(mouseX, mouseY, e) {
-		this.map_button.handleHover(mouseX, mouseY, e)
+		if (!this.game.player.inventory.isOpen) {
+			this.map_button.handleHover(mouseX, mouseY, e)
+		}
+		if (this.tradeWindow) {
+			this.tradeWindow.mouseMove(mouseX, mouseY, e)
+		}
+	}
+	mouseUp(mouseX, mouseY, e) {
+		if (this.tradeWindow) {
+			this.tradeWindow.mouseUp(mouseX, mouseY, e)
+		}
 	}
 }
